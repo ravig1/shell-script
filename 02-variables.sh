0db36b80a2b92ff2151ed87325f6386ce4c2195e
ravig1 <raviteja2341996@gmail.com>
2024-05-31 04:39:43 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$person1: Hello $Person2, Good Morning"
-echo "$PERSON2:Hi $PERSON1, Very Good Morning"
-echo "$PERSON1: How are you $PERSON2 ?"
-ECHO "$PERSON1: I am good $PERSON1, How are you"
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON1: I am good $PERSON1, How are you"
