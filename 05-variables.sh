b5ab48f7bf3d5a433d3ce2975efd035237157d11
ravig1 <raviteja2341996@gmail.com>
2024-05-31 05:57:12 +0530

    variables 4

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# read -s is used to not echo input coming from a terminal 
+
+echo "Please enter your username:: "
+
+read -s USERNAME #the value entered above will be automatically attached to USERNAME variable
+
+echo "Please enter your Password:: "
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD" # i am printing just for validation, you shoud not print username and passwords in scripts 
